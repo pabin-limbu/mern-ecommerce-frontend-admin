36dff0284333a248468a995c1cf76962aee3eb3a
pabin-limbu <changsu.pabin@gmail.com>
2021-08-26 16:41:05 +0800

    Url changed from local to heroku url

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,4 @@
-export const api = "http://localhost:2000/api";
+export const api = "https://ecom-rest-server.herokuapp.com/api";
 export const generatePublicUrl = (fileName) => {
   return `http://localhost:2000/public/${fileName}`;
 };
